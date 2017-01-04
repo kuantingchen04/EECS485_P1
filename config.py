5b06c0fdf48d266d1077a97c059bd152c3be1870
Claire Rehfuss <crehfuss@Claires-MacBook-Pro.local>
2017-01-04 13:58:36 -0500

    First commit

---
+++ config.py
@@ -0,0 +1,10 @@
+# Do NOT commit this file to github
+# Make a seperate one for your deployed environment
+# Do not change the host
+env = dict(
+	host = '0.0.0.0',
+	port = 3000,
+	user = 'root', 
+	password = '',
+	db = 'test_db',
+)
