c948860718c1beae314fda3ce867b8390fe2f6e7
frank <tennis82517@0587375735.wireless.umich.net>
2017-01-08 14:56:30 -0500

    first launch

--- controllers/main.py
+++ controllers/main.py
@@ -6,7 +6,7 @@ main = Blueprint('main', __name__, template_folder='templates')
 
 @main.route('/')
 def main_route():
-    return render_template("index.html")
+    return render_template("files.php")
 
 @main.route('/hello')
 def main_hello():
