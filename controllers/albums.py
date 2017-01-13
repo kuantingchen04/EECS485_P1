2c8bc4bf721a235d728457a6dbde56d271ed4056
WenJenHsieh <tennis82517@frankdeMacBook-Pro.local>
2017-01-13 00:05:39 -0500

    widely using url_for

--- controllers/albums.py
+++ controllers/albums.py
@@ -3,10 +3,7 @@ from extensions import connect_to_database
 albums = Blueprint('albums', __name__, template_folder='templates')
 #albums_dict={'sportslover':[['sports','I love sports','1'],['football','I love fot ball','2']],'traveler':[['world','Around the world','3']],'spacejunkie':[['space','Cool space shots','4']]}
 def get_album_list(username):
-	db = connect_to_database()
-	cur = db.cursor()
-	cur.execute('select albumid, title ,username from Album WHERE username = \''+username+'\';')
-	results=cur.fetchall()
+	results=access_db('select albumid, title ,username from Album WHERE username = \''+username+'\';')
 	album_list=[[r['albumid'],r['title']] for r in results]
 	return album_list
 def access_db(query):
@@ -22,11 +19,12 @@ def albums_edit_route():
 		if request.form.get("op")=="add":
 			max_albumid=(access_db("SELECT MAX(albumid) FROM Album")[0]['MAX(albumid)'])
 			access_db("INSERT INTO Album (albumid, title, created, lastupdated, username) VALUES ("+str(max_albumid+1)+",\""+request.form.get("title")+"\",TIMESTAMP '2017-01-11 03:56:35',TIMESTAMP '2017-01-11 03:56:35',\'"+request.form.get("username")+"\');")
-			return redirect('/albums/edit?username='+request.form.get("username"))
+			return redirect(url_for('albums.albums_edit_route',username=request.form.get("username")))
 		elif request.form.get("op")=="delete":
-			print("DELETE FROM Album WHERE albumid = \""+request.form.get("albumid")+"\"")
+			results=access_db("SELECT albumid , username FROM Album WHERE albumid = \""+request.form.get("albumid")+"\"")
+			username=results[0]["username"]
 			access_db("DELETE FROM Album WHERE albumid = \""+request.form.get("albumid")+"\"")
-			return redirect('/albums/edit?username='+request.args.get("username"))
+			return redirect(url_for ('albums.albums_edit_route',username=username))
 		
 	username=request.args.get("username")
 	
