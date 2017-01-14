fcb96cd4b8ad8117dc2c613d4d81df9623ca3346
WenJenHsieh <tennis82517@frankdeMacBook-Pro.local>
2017-01-14 00:11:03 -0500

    The nasty sql things

--- sql_output_request.sh
+++ sql_output_request.sh
@@ -1,4 +1,4 @@
-mysql -u group32 group32db -p group32pw!<sql_request_1.sql>sql_output_1.txt
-mysql -u group32 group32db -p group32pw!<sql_request_2.sql>sql_output_2.txt
-mysql -u group32 group32db -p group32pw!<sql_request_3.sql>sql_output_3.txt
-mysql -u group32 group32db -p group32pw!<sql_request_4.sql>sql_output_4.txt
+mysql -u group32 group32db -p <sql_request_1.sql>sql_output_1.txt
+mysql -u group32 group32db -p <sql_request_2.sql>sql_output_2.txt
+mysql -u group32 group32db -p <sql_request_3.sql>sql_output_3.txt
+mysql -u group32 group32db -p <sql_request_4.sql>sql_output_4.txt
