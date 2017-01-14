1c651bad76b2d6fc749d11777a2987209b582ffb
WenJenHsieh <tennis82517@frankdeMacBook-Pro.local>
2017-01-13 23:34:31 -0500

    add the sql output

--- sql_output.sql
+++ sql_output.sql
@@ -1,4 +1,4 @@
-SELECT (username, firstname, lastname, password, email) FROM User ORDER BY username;
-SELECT (albumid, title, username) FROM Album ORDER BY albumid;
+SELECT username, firstname, lastname, password, email FROM User ORDER BY username;
+SELECT albumid, title, username FROM Album ORDER BY albumid;
 SELECT picid, format FROM Photo ORDER BY picid;
 SELECT albumid, picid, caption FROM Contain ORDER BY picid;
